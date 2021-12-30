66c3c9b5c1515c8a24aee3f26cc047cb486ce507
enkudavi <ekopur05@gmail.com>
2021-12-30 17:41:00 +0700

    ganti pp dan portfolio

--- src/components/ProjectsGrid.vue
+++ src/components/ProjectsGrid.vue
@@ -115,26 +115,26 @@ export default {
 			projects: [
 				{
 					id: 1,
-					title: 'Google Health Platform',
+					title: 'Portal Monitoring System for Agent',
 					category: 'Web Application',
 					img: require('@/assets/images/web-project-2.jpg'),
 				},
 				{
 					id: 2,
-					title: 'Phoenix Digital Agency',
-					category: 'Mobile Application',
+					title: 'Real Time Monitoring Active Call',
+					category: 'Web Application',
 					img: require('@/assets/images/mobile-project-2.jpg'),
 				},
 				{
 					id: 3,
-					title: 'Project Management UI',
-					category: 'UI/UX Design',
+					title: 'Mazeko Blog',
+					category: 'Blog',
 					img: require('@/assets/images/ui-project-1.jpg'),
 				},
 				{
 					id: 4,
-					title: 'Cloud Storage Platform',
-					category: 'UI/UX Design',
+					title: 'Toko Online Sepatu',
+					category: 'Mobile Application',
 					img: require('@/assets/images/ui-project-2.jpg'),
 				},
 				{
